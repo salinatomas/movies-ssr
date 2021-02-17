bbd0e9165945d5437c9083cad7ed1a8ab3c90fb3
Tomás Salina <salinatomass53@gmail.com>
2021-02-17 00:44:33 -0300

    modularized server.js:renderApp

--- src/server/server.js
+++ src/server/server.js
@@ -3,27 +3,16 @@ import dotenv from "dotenv";
 import webpack from "webpack";
 import helmet from "helmet";
 
-import React from "react";
-import { renderToString } from "react-dom/server";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import { renderRoutes } from "react-router-config";
-import { StaticRouter } from "react-router-dom";
-import Layout from "../frontend/components/Layout";
-import serverRoutes from "../frontend/routes/serverRoutes";
-
-import reducer from "../frontend/reducers";
-
-import getManifest from "./getManifest";
-
 import coockieParser from "cookie-parser";
 import passport from "passport";
 import session from "express-session";
-import axios from "axios";
-import jwt from "jsonwebtoken";
+
+import getManifest from "./utils/getManifest";
 
 import userMovies from "./routes/userMovies";
 import auth from "./routes/auth";
+
+import renderApp from "./utils/renderApp";
 
 dotenv.config();
 
@@ -59,122 +48,13 @@ if (ENV === "development") {
   app.use(helmet.permittedCrossDomainPolicies());
 }
 
-const setResponse = (html, preloadedState, manifest) => {
-  const mainStyles = manifest ? manifest["main.css"] : "assets/app.css";
-  const mainBuild = manifest ? manifest["main.js"] : "assets/app.js";
-
-  return `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Platzi Video</title>
-        <link rel="stylesheet" href=${mainStyles} type="text/css" />
-      </head>
-      <body>
-        <div id="app">
-          ${html}
-        </div>
-        <script>
-          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(
-            /</g,
-            "\\u003c"
-          )}
-        </script>
-        <script src=${mainBuild} type="text/javascript"></script>
-      </body>
-    </html>
-  `;
-};
-
-const renderApp = async (req, res) => {
-  let initialState;
-
-  const { token } = req.cookies;
-  const verifyToken = jwt.decode(token) || {
-    sub: { email: undefined, name: undefined, id: undefined },
-  };
-
-  const email = req.cookies.email || verifyToken.sub.email;
-  const name = req.cookies.name || verifyToken.sub.name;
-  const id = req.cookies.id || verifyToken.sub.id;
-
-  try {
-    let { data: userMovies } = await axios({
-      url: `${process.env.API_URL}/api/user-movies?userId=${id}`,
-      headers: { Authorization: `Bearer ${token}` },
-      method: "get",
-    });
-
-    userMovies = userMovies.data;
-
-    let { data: movieList } = await axios({
-      url: `${process.env.API_URL}/api/movies`,
-      headers: { Authorization: `Bearer ${token}` },
-      method: "get",
-    });
-
-    movieList = movieList.data;
-
-    initialState = {
-      user: {
-        email,
-        name,
-        id,
-      },
-      playing: {},
-      myList: userMovies.map((userMovie) => {
-        const favoriteMovie = movieList.find(
-          (item) => item._id === userMovie.movieId
-        );
-
-        return {
-          ...favoriteMovie,
-          userMovieId: userMovie._id,
-        };
-      }),
-      trends: movieList.filter(
-        (movie) => movie.contentRating === "PG" && movie._id
-      ),
-      originals: movieList.filter(
-        (movie) => movie.contentRating === "G" && movie._id
-      ),
-    };
-  } catch (err) {
-    initialState = {
-      user: {},
-      playing: {},
-      myList: [],
-      trends: [],
-      originals: [],
-    };
-  }
-
-  const store = createStore(reducer, initialState);
-  const preloadedState = store.getState();
-  const isLogged = initialState.user.id;
-  const html = renderToString(
-    <Provider store={store}>
-      <StaticRouter location={req.url} context={{}}>
-        <Layout>{renderRoutes(serverRoutes(isLogged))}</Layout>
-      </StaticRouter>
-    </Provider>
-  );
-
-  res.removeHeader("x-powered-by");
-  res.send(setResponse(html, preloadedState, req.hashManifest));
-};
-
 // routes
 userMovies(app);
 auth(app);
 app.get("*", renderApp);
 
 app.listen(PORT, (err) => {
-  if (err) {
-    return console.log(err);
-  }
+  if (err) console.log(err);
 
   console.log(`Server running in http://localhost:${PORT}`);
 });
--- src/server/utils/getManifest.js
+++ src/server/utils/getManifest.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 const getManifest = () => {
   try {
-    return JSON.parse(fs.readFileSync(`${__dirname}/public/manifest.json`));
+    return JSON.parse(fs.readFileSync(`${__dirname}/../public/manifest.json`));
   } catch (err) {
     console.log(err);
   }
